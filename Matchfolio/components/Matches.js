e77f72a6a485c28c190b389ec5e5d17cadd833b6
Simon Wong <thatonesimon@gmail.com>
2018-02-01 13:34:57 -0800

    Matched properties page is able to toggle from map to list view

--- Matchfolio/components/Matches.js
+++ Matchfolio/components/Matches.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { AsyncStorage } from 'react-native'
+import { AsyncStorage, StyleSheet } from 'react-native'
 import { Drawer,
          Container,
          Header,
@@ -21,6 +21,8 @@ import { Drawer,
          Spinner,
          List,
          ListItem } from 'native-base';
+import ActionButton from 'react-native-action-button';
+import MapView from 'react-native-maps';
 
 const baseUrl = 'http://pa.cdn.appfolio.com/';
 
@@ -40,6 +42,8 @@ export default class Matches extends Component {
 	constructor(props){
 		super(props);
 		this._onRenderRow = this._onRenderRow.bind(this);
+        // start with map
+        this.state= {listView: false};
 	}
 
    componentWillMount(){
@@ -49,7 +53,7 @@ export default class Matches extends Component {
 	async checkMatchedProperties(){
 		try {
 			var matches = await AsyncStorage.getItem('matched-properties');
-			
+
 			if (value !== null){
 				this.setState({matches: JSON.parse(matches)});
 			}
@@ -57,7 +61,7 @@ export default class Matches extends Component {
 				console.log("No matched properties data file!");
 			}
 		}
-		
+
 		catch (error){
 			console.log(error);
 		}
@@ -88,37 +92,108 @@ export default class Matches extends Component {
          if (this.props.navigation.state.params.remainingInfos)
             remainingInfos = this.props.navigation.state.params.remainingInfos;
       }
-    return (
-      <Container backgroundColor='white'>
-          <Header>
-            <Left>
-              <Button transparent
-              onPress={()=> this.props.navigation.navigate('DrawerToggle')}>
-                <Icon name='menu' />
-              </Button>
-            </Left>
-            <Body>
-              <Title>MatchFolio</Title>
-            </Body>
-            <Right />
-          </Header>
-        <Content>
-         <List dataArray={data}
-            renderRow={this._onRenderRow} /> 
-        </Content>
-            <Footer>
-              <FooterTab>
-                <Button vertical onPress={()=>this.props.navigation.navigate('home', {homeSavedMatches: data, remainingInfos: remainingInfos})}>
-                  <Icon name="search" />
-                  <Text>Find Properties</Text>
-                </Button>
-                <Button vertical active>
-                  <Icon active name="home" />
-                  <Text>Matched Properties</Text>
-                </Button>
-              </FooterTab>
-            </Footer>
-      </Container>
-    );
+      if(this.state.listView) {
+          return (
+            <Container backgroundColor='white'>
+                <Header>
+                  <Left>
+                    <Button transparent
+                    onPress={()=> this.props.navigation.navigate('DrawerToggle')}>
+                      <Icon name='menu' />
+                    </Button>
+                  </Left>
+                  <Body>
+                    <Title>MatchFolio</Title>
+                  </Body>
+                  <Right />
+                </Header>
+              <Content>
+               <List dataArray={data}
+                  renderRow={this._onRenderRow} />
+              </Content>
+              <ActionButton
+                buttonColor="rgba(231,76,60,1)"
+                onPress={() => { this.setState({listView: !this.state.listView})}}
+                position="right"
+                style={styles.button}
+              />
+                  <Footer>
+                    <FooterTab>
+                      <Button vertical onPress={()=>this.props.navigation.navigate('home', {homeSavedMatches: data, remainingInfos: remainingInfos})}>
+                        <Icon name="search" />
+                        <Text>Find Properties</Text>
+                      </Button>
+                      <Button vertical active>
+                        <Icon active name="home" />
+                        <Text>Matched Properties</Text>
+                      </Button>
+                    </FooterTab>
+                  </Footer>
+            </Container>
+
+          );
+      }
+      else {
+          return (
+
+              <Container backgroundColor='white'>
+                  <Header>
+                    <Left>
+                      <Button transparent
+                      onPress={()=> this.props.navigation.navigate('DrawerToggle')}>
+                        <Icon name='menu' />
+                      </Button>
+                    </Left>
+                    <Body>
+                      <Title>MatchFolio</Title>
+                    </Body>
+                    <Right />
+                  </Header>
+                <Content style={{flexDirection:"column"}}>
+                <MapView
+                    initialRegion={{
+                      latitude: 34.434248,
+                      longitude: -119.863704,
+                      latitudeDelta: 0.0922,
+                      longitudeDelta: 0.0421,
+                    }}
+                    style={styles.map}
+                    />
+                </Content>
+                <ActionButton
+                  buttonColor="rgba(231,76,60,1)"
+                  onPress={() => { this.setState({listView: !this.state.listView})}}
+                  position="right"
+                  style={styles.button}
+                />
+                    <Footer>
+                      <FooterTab>
+                        <Button vertical onPress={()=>this.props.navigation.navigate('home', {homeSavedMatches: data, remainingInfos: remainingInfos})}>
+                          <Icon name="search" />
+                          <Text>Find Properties</Text>
+                        </Button>
+                        <Button vertical active>
+                          <Icon active name="home" />
+                          <Text>Matched Properties</Text>
+                        </Button>
+                      </FooterTab>
+                    </Footer>
+              </Container>
+
+          );
+      }
+
   }
 }
+
+const styles = StyleSheet.create({
+    map: {
+        height: 1000,
+        flex: 1,
+        marginBottom: 10,
+        borderRadius: 5,
+    },
+    button: {
+        marginBottom: 50,
+    }
+});
