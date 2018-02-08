b70010db07fea70c38188dc904dbedadfb9e8362
Simon Wong <thatonesimon@gmail.com>
2018-02-08 14:46:54 -0800

    More work for additional questions page

--- Matchfolio/components/AdditionalQuestions.js
+++ Matchfolio/components/AdditionalQuestions.js
@@ -1,16 +1,6 @@
 import React, { Component } from 'react';
-import { Alert, Dimensions, Image, ScrollView, StyleSheet, } from 'react-native';
-import { Button,
-         Drawer,
-         Container,
-         Header,
-         View,
-         DeckSwiper,
-         Card,
-         CardItem,
-         Thumbnail,
-         Text,
-         Spinner, } from 'native-base';
+import { Alert, ScrollView, StyleSheet } from 'react-native';
+import { Button, Container, Text, Form, Item, Label, View } from 'native-base';
 
 
 export default class AdditionalQuestions extends Component {
@@ -20,17 +10,48 @@ export default class AdditionalQuestions extends Component {
         if(this.props.navigation.state.params) {
           questions = this.props.navigation.state.params.questions;
         }
+        additionalQuestions = "This property has additional questions on it's application!";
+    }
+
+    _apply() {
+        Alert.alert("Your application has been completed!");
+        this.props.navigation.navigate('main');
     }
 
     render() {
         return (
-            <ScrollView>
-                <Text>This property has additional questions on it's application!</Text>
+            <ScrollView style={styles.holder}>
+                <Text>{additionalQuestions}</Text>
+                <Form style={styles.form}>
+                {questions.map(question => (
+                    <Item floatingLabel key={question}>
+                      <Label>{question}</Label>
+                    </Item>
+                ))}
+                </Form>
+                <View style={{flexDirection: 'row', flex: 1}}>
+                    <Button success style={{flex: 1}} onPress={() => this._apply() } >
+                      <Text style={{textAlign: 'center'}}>Apply to Property</Text>
+                    </Button>
+                </View>
             </ScrollView>
 
-        )
+        );
     }
-
-
-
 }
+
+const styles = StyleSheet.create({
+    holder: {
+        flex: 1,
+        margin: 10,
+    },
+    form: {
+        marginTop: 10,
+        marginBottom: 10,
+        borderWidth: 1,
+        backgroundColor: '#eef8fd',
+        borderRadius: 5,
+        paddingBottom: 10,
+        paddingRight: 10,
+    },
+});
