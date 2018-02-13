4330baf6316f952332061746cc75b97ae1e3ffcb
Simon Wong <thatonesimon@gmail.com>
2018-02-13 14:45:44 -0800

    Added some parsing for Additional Questions

--- Matchfolio/components/AdditionalQuestions.js
+++ Matchfolio/components/AdditionalQuestions.js
@@ -8,7 +8,7 @@ export default class AdditionalQuestions extends Component {
     constructor(props) {
         super(props);
         if(this.props.navigation.state.params) {
-          questions = this.props.navigation.state.params.questions;
+          questions = this.props.navigation.state.params.questions.split("~");
         }
         additionalQuestions = "This property has additional questions on it's application!";
     }
