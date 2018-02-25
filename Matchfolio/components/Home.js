8de9776852087258a03d0a51f6580e666e9dc0a6
Chantel <ucsb2@169-231-111-152.wireless.ucsb.edu>
2018-02-25 14:18:24 -0800

    fixed styles

--- Matchfolio/components/Home.js
+++ Matchfolio/components/Home.js
@@ -45,52 +45,52 @@ import { Drawer,
       drawerIcon: ({ tintColor }) => (<Icon name="home" size={15} style={{ color: tintColor }} />),
     }
 
-	constructor(props){
-		super(props);
-		this.state = {loading: true, matches: null};
-		this._onNotInterested = this._onNotInterested.bind(this);
-		this._nextProperty = this._nextProperty.bind(this);
-		this._onInterested = this._onInterested.bind(this);
-		this._onMoreInfo = this._onMoreInfo.bind(this);
-	}
+    constructor(props){
+      super(props);
+      this.state = {loading: true, matches: null};
+      this._onNotInterested = this._onNotInterested.bind(this);
+      this._nextProperty = this._nextProperty.bind(this);
+      this._onInterested = this._onInterested.bind(this);
+      this._onMoreInfo = this._onMoreInfo.bind(this);
+    }
 
-  async componentWillMount() {
-    await Expo.Font.loadAsync({
-      'Ionicons': require('native-base/Fonts/Ionicons.ttf'),
-      'Roboto': require('native-base/Fonts/Roboto.ttf'),
-      'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
-    });
-    /*
-    await propertyRef.once('value').then(function(dataSnapshot) {
+    async componentWillMount() {
+      await Expo.Font.loadAsync({
+        'Ionicons': require('native-base/Fonts/Ionicons.ttf'),
+        'Roboto': require('native-base/Fonts/Roboto.ttf'),
+        'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+      });
+      /*
+      await propertyRef.once('value').then(function(dataSnapshot) {
       propertyInfo = dataSnapshot.val();
       //Make sure this function callback does not happen after the checking of the state params below
       //which should hold precedence for assigning remainingInfos
       remainingInfos = propertyInfo.slice();
       console.log('propertyinfo init')
-    }, function(error){
+      }, function(error){
       console.log(error.message)
-    })\*/
-    propertyInfo = global.UserPropertyListing;
-    remainingInfos = global.UserPropertyListing;
+      })\*/
+      propertyInfo = global.UserPropertyListing;
+      remainingInfos = global.UserPropertyListing;
 
-    user = firebase.auth().currentUser;
-    userDataRef = firebase.database().ref("users/" +user.uid);
+      user = firebase.auth().currentUser;
+      userDataRef = firebase.database().ref("users/" +user.uid);
 
-    if(user) {
-      console.log("dispname: ", user.displayName);
-      console.log("email: ", user.email);
-      console.log("uid: ", user.uid);
-    }
-    else {
-      console.log("user data not loaded");
-    }
-    this.setState({matches: global.matched})
-    if (this.props.navigation.state.params) {
-      if(this.props.navigation.state.params.homeSavedMatches)
+      if(user) {
+        console.log("dispname: ", user.displayName);
+        console.log("email: ", user.email);
+        console.log("uid: ", user.uid);
+      }
+      else {
+        console.log("user data not loaded");
+      }
+      this.setState({matches: global.matched})
+      if (this.props.navigation.state.params) {
+        if(this.props.navigation.state.params.homeSavedMatches)
         this.setState({matches: this.props.navigation.state.params.homeSavedMatches});
-      if(this.props.navigation.state.params.remainingInfos)
+        if(this.props.navigation.state.params.remainingInfos)
         remainingInfos = this.props.navigation.state.params.remainingInfos;
-    }
+      }
 
       this.setState({loading: false});
     }
@@ -106,13 +106,13 @@ import { Drawer,
       this._onNotInterested(this.deck._root.state.selectedItem);
     }
 
-  _onNotInterested(item) {
-    // [maybe] save info to ensure property isn't displayed again
-    if(item!=null){
-      this._removeInfoFromRemaining(item);
-      userDataRef.child("uninterested").child(item.listable_uid).set(1);
+    _onNotInterested(item) {
+      // [maybe] save info to ensure property isn't displayed again
+      if(item!=null){
+        this._removeInfoFromRemaining(item);
+        userDataRef.child("uninterested").child(item.listable_uid).set(1);
+      }
     }
-  }
 
     _onMoreInfo() {
       if(this.deck._root.state.selectedItem!=null && remainingInfos.length>0)
@@ -128,19 +128,19 @@ import { Drawer,
       if(item == null || remainingInfos.length<1)
       return;
 
-    var newArray = [];
-    if (this.state.matches!=null)
-    {
-      //make shallow copy as to not modify original
-      newArray = this.state.matches.slice();
-    }
+      var newArray = [];
+      if (this.state.matches!=null)
+      {
+        //make shallow copy as to not modify original
+        newArray = this.state.matches.slice();
+      }
 
-		newArray.push(item);
-		this.setState({matches: newArray})
-		this._removeInfoFromRemaining(item);
-    userDataRef.child("interested").child(item.listable_uid).set(1);
-		// go on to next property
-	}
+      newArray.push(item);
+      this.setState({matches: newArray})
+      this._removeInfoFromRemaining(item);
+      userDataRef.child("interested").child(item.listable_uid).set(1);
+      // go on to next property
+    }
 
     _updatePropertyImages(index) {
       // propertyPictures = propertyInfo[index].image_urls.split(",");
@@ -155,18 +155,18 @@ import { Drawer,
       }
     }
 
- 	render() {
-		if (this.state.loading || propertyInfo === undefined) {
-			return (
-        <View style ={styles.containerSpin}>
-          <Spinner color='#006eff'/>
-        </View>
-      )
-		}
+    render() {
+      if (this.state.loading || propertyInfo === undefined) {
+        return (
+          <View style ={styles.containerSpin}>
+            <Spinner color='#006eff'/>
+          </View>
+        )
+      }
 
-		else {
-      //remainingInfos = propertyInfo.slice();
-			this._updatePropertyImages(this.state.currentProperty);
+      else {
+        //remainingInfos = propertyInfo.slice();
+        this._updatePropertyImages(this.state.currentProperty);
 
         return (
           <Container style={{backgroundColor: 'white'}}>
@@ -257,55 +257,106 @@ import { Drawer,
 }
 }
 const styles = StyleSheet.create({
-   container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    containerSpin: {
-      flex: 1,
-      flexDirection: 'row',
-      justifyContent: 'center',
-      alignItems: 'center',
-      alignSelf: 'center'
-    },
-    horizontalHolder: {
-      flexDirection: 'row',
-      alignItems: 'center',
-      justifyContent: 'space-between',
-      padding: 10,
-    },
-    propertyInfo: {
-      flex: 1,
-      textAlign: 'center',
-      fontSize: 15,
-    },
-    leftPropertyInfo: {
-      flex: 1,
-      textAlign: 'center',
-      fontSize: 15,
-      borderRightColor: 'black',
-      borderRightWidth: 1,
-    },
-    rightPropertyInfo: {
-      flex: 1,
-      textAlign: 'center',
-      fontSize: 15,
-      borderLeftColor: 'black',
-      borderLeftWidth: 1,
-    },
-    propertyDescription: {
-      flex: 1,
-      textAlign: 'center',
-      fontSize: 20,
-    },
-    header: {
-      fontSize: 20,
-    },
-    phoneNumber: {
-      flex: 1,
-      textAlign: 'center',
-      color: '#aaa',
-    },
-  });
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  containerSpin: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center'
+  },
+  horizontalHolder: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: 10,
+  },
+  propertyInfo: {
+    flex: 1,
+    textAlign: 'center',
+    fontSize: 15,
+  },
+  leftPropertyInfo: {
+    flex: 1,
+    textAlign: 'center',
+    fontSize: 15,
+    borderRightColor: 'black',
+    borderRightWidth: 1,
+  },
+  rentPropertyInfo: {
+    flex: 3,
+    textAlign: 'left',
+    fontSize: 24,
+    borderRightColor: 'black',
+    borderRightWidth: 1,
+  },
+  otherPropertyInfo: {
+    flex: 1,
+    textAlign: 'center',
+    fontSize: 16,
+    borderLeftColor: 'black',
+    borderLeftWidth: 1,
+  },
+  propertyDescription: {
+    flex: 1,
+    textAlign: 'center',
+    fontSize: 20,
+  },
+  header: {
+    fontSize: 20,
+  },
+  phoneNumber: {
+    flex: 1,
+    textAlign: 'center',
+    color: '#aaa',
+  },
+});
+//   container: {
+  //     flex: 1,
+  //     backgroundColor: '#fff',
+  //     alignItems: 'center',
+  //     justifyContent: 'center',
+  //   },
+  //   horizontalHolder: {
+  //     flexDirection: 'row',
+  //     textAlignVertical: 'center',
+  //     justifyContent: 'space-between',
+  //   },
+  //   verticalHolder: {
+  //     flexDirection: 'column',
+  //     justifyContent: 'space-between',
+  //     padding: 2,
+  //   },
+  //   rentPropertyInfo: {
+  //     flex: 3,
+  //     textAlign: 'left',
+  //     fontSize: 24,
+  //     borderRightColor: 'black',
+  //     borderRightWidth: 1,
+  //   },
+  //   otherPropertyInfo: {
+  //     flex: 1,
+  //     textAlign: 'center',
+  //     fontSize: 16,
+  //     borderLeftColor: 'black',
+  //     borderLeftWidth: 1,
+  //   },
+  //   propertyDescription: {
+  //     flex: 1,
+  //     textAlign: 'center',
+  //     fontSize: 20,
+  //   },
+  //   header: {
+  //     fontSize: 20,
+  //   },
+  //   phoneNumber: {
+  //     flex: 1,
+  //     textAlign: 'center',
+  //     color: '#aaa',
+  //   },
+  // });
