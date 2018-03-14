387b682d02fb12f3d44f55e88bd6ef5748d94020
Peter Master <petehoke@comcast.net>
2018-03-13 17:10:09 -0700

    Add placeholder values to rental app

--- Matchfolio/components/RentalApp.js
+++ Matchfolio/components/RentalApp.js
@@ -68,38 +68,68 @@ export default class App extends Component<{}> {
            <Text style={styles.header}><Ionicons name="ios-list-box-outline" size={30} color="skyblue" padding = {40}/> Rental Application</Text>
           </Body>
           <Right style={{flex: 1}} />
-				</Header>
+	</Header>
         <Content>
           <Form>
             <Item floatingLabel>
+              <Label>Phone Number</Label>
+              <Input placeholder="123-456-7890"/>
+            </Item>
+            <Item floatingLabel>
+              <Label>Driver's License Number</Label>
+              <Input placeholder="A1234567"/>
+            </Item>
+            <Item floatingLabel>
+              <Label>SSN</Label>
+              <Input placeholder="123456789"/>
+            </Item>
+            <Item floatingLabel>
               <Label>Previous Address (1st line)</Label>
+              <Input placeholder="123 Street Dr."/>
             </Item>
             <Item floatingLabel>
               <Label>Previous Address (2nd line)</Label>
+              <Input placeholder="New York, NY 13579"/>
             </Item>
             <Item floatingLabel>
               <Label>Previous Rent</Label>
+              <Input placeholder="$700"/>
             </Item>
             <Item floatingLabel>
               <Label>Annualized Salary</Label>
+              <Input placeholder="$27000"/>
             </Item>
             <Item floatingLabel>
               <Label>Occupation</Label>
+              <Input placeholder="Barista"/>
             </Item>
             <Item floatingLabel>
               <Label>Employer</Label>
+              <Input placeholder="Starbucks"/>
             </Item>
-	    <Item floatingLabel>
-              <Label>Vehicle Make, Model, Year</Label>
+            <Item floatingLabel>
+              <Label>Vehicle Make</Label>
+              <Input placeholder="Chevy"/>
+            </Item>
+            <Item floatingLabel>
+              <Label>Vehicle Model</Label>
+              <Input placeholder="Malibu"/>
+            </Item>
+            <Item floatingLabel>
+              <Label>Vehicle Year</Label>
+              <Input placeholder="2006"/>
             </Item>
             <Item floatingLabel>
               <Label>Savings Account Balance</Label>
+              <Input placeholder="$8000"/>
             </Item>
             <Item floatingLabel>
               <Label>Checking Account Balance</Label>
+              <Input placeholder="$400"/>
             </Item>
             <Item floatingLabel>
               <Label>Credit Card Debt and Loan Balance</Label>
+              <Input placeholder="$2700"/>
             </Item>
           </Form>
           <Button bordered block onPress={() => this.props.navigation.navigate('backgroundcheck')} style={styles.button} >
